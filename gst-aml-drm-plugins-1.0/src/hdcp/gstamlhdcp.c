9df8039ee5504667d7731064c90538201dde02b6
bo.li6 <bo.li6@amlogic.com>
2022-03-10 19:41:48 +0800

    gstamlhdcp: amlhdcp support audio decrypt optimize autoplugin [1/1]
    PD#OTT-28518
    
    Problem:
    sony hdcp audio is encryp data
    
    Solution:
    support audio decrypt optimize autoplugin
    
    Verify:
    Y4+Yocto
    
    Change-Id: I1b78a4a345c2baa45c44ecbc6b3901d430257bce
    Signed-off-by: bo.li6 <bo.li6@amlogic.com>

--- gst-aml-drm-plugins-1.0/src/hdcp/gstamlhdcp.c
+++ gst-aml-drm-plugins-1.0/src/hdcp/gstamlhdcp.c
@@ -22,7 +22,8 @@ GST_DEBUG_CATEGORY_STATIC (gst_aml_hdcp_debug);
 static GstStaticPadTemplate sinktemplate = GST_STATIC_PAD_TEMPLATE ("sink",
     GST_PAD_SINK,
     GST_PAD_ALWAYS,
-    GST_STATIC_CAPS ("application/x-hdcp"));
+    GST_STATIC_CAPS ("application/x-hdcp, original-media-type=(string)video/x-h264; "
+                     "application/x-hdcp, original-media-type=(string)audio/mpeg"));
 
 static GstStaticPadTemplate srctemplate = GST_STATIC_PAD_TEMPLATE ("src",
     GST_PAD_SRC,
@@ -76,7 +77,7 @@ gst_aml_hdcp_class_init (GstAmlHDCPClass * klass)
 
     gst_element_class_set_details_simple(element_class,
             "Amlogic HDCP Plugin",
-            "Decryptor/Converter/Video/DRM/HDCP",
+            "Decryptor/Converter/DRM/HDCP",
             "HDCP Decryption Plugin",
             "mm@amlogic.com");
 }
@@ -93,7 +94,6 @@ gst_aml_hdcp_init(GstAmlHDCP * plugin)
     plugin->hdcp_init_done = -1;
     plugin->hdcp_context = NULL;
     plugin->hdcp_handle = NULL;
-
 }
 
 void
@@ -120,14 +120,25 @@ gst_aml_hdcp_set_property(GObject * object, guint prop_id, const GValue * value,
 void
 gst_aml_hdcp_get_property(GObject * object, guint prop_id, GValue * value, GParamSpec * pspec)
 {
+    GstAmlHDCP *plugin = GST_AMLHDCP(object);
+
+    switch (prop_id) {
+    case PROP_DRM_STATIC_PIPELINE:
+    break;
+    case PROP_DRM_HDCP_CONTEXT:
+    break;
+    default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+        break;
+    }
 }
 
 gboolean
 gst_aml_hdcp_start(GstBaseTransform *trans)
 {
-
     return TRUE;
 }
+
 gboolean
 gst_aml_hdcp_stop(GstBaseTransform *trans)
 {
@@ -143,84 +154,132 @@ gst_aml_hdcp_stop(GstBaseTransform *trans)
     return TRUE;
 }
 
+/*
+  Append new_structure to dest, but only if it does not already exist in res.
+  This function takes ownership of new_structure.
+*/
+static bool AppendIfNotDuplicate(GstCaps* destination, GstStructure* structure)
+{
+    bool duplicate = false;
+    unsigned size = gst_caps_get_size(destination);
+    for (unsigned index = 0; !duplicate && index < size; ++index) {
+        GstStructure* s = gst_caps_get_structure(destination, index);
+        if (gst_structure_is_equal(s, structure))
+            duplicate = true;
+    }
+
+    if (!duplicate)
+        gst_caps_append_structure(destination, structure);
+    else
+        gst_structure_free(structure);
+
+    return duplicate;
+}
+
 GstCaps*
 gst_aml_hdcp_transform_caps(GstBaseTransform *trans, GstPadDirection direction,
         GstCaps *caps, GstCaps *filter)
 {
     GstAmlHDCP *plugin = GST_AMLHDCP(trans);
-    GstCaps *srccaps, *sinkcaps;
-    GstCaps *ret = NULL;
+    GstCaps *transformed_caps = gst_caps_new_empty();
+    guint incoming_caps_size;
+    guint i;
+    guint index;
+    gboolean isvideo = TRUE;
 
-    GST_DEBUG_OBJECT(plugin, "transform_caps direction:%d filter:%" GST_PTR_FORMAT, direction, filter);
+    if (direction == GST_PAD_UNKNOWN)
+        return NULL;
 
-    srccaps = gst_pad_get_pad_template_caps(GST_BASE_TRANSFORM_SRC_PAD(trans));
-    sinkcaps = gst_pad_get_pad_template_caps(GST_BASE_TRANSFORM_SINK_PAD(trans));
+    GST_DEBUG_OBJECT (plugin,
+        "direction: %s, caps: %" GST_PTR_FORMAT " filter: %" GST_PTR_FORMAT,
+        (direction == GST_PAD_SRC) ? "src" : "sink", caps, filter);
 
-  switch (direction) {
-    case GST_PAD_SINK:
-    {
-       if (gst_caps_can_intersect(caps, sinkcaps)) {
-            gboolean find = false;
-            unsigned size;
-            ret = gst_caps_copy(caps);
-            size = gst_caps_get_size(ret);
-            for (unsigned i = 0; i < size; ++i) {
-                GstStructure *structure;
-                structure = gst_caps_get_structure(caps, i);
-                if (g_str_has_prefix(gst_structure_get_name(structure), "application/")) {
-                    find = true;
-                    ret = gst_caps_new_simple("video/x-h264",
-                        "stream-format", G_TYPE_STRING, "byte-stream",
-                        "alignment", G_TYPE_STRING, "nal", NULL);
-                    gst_caps_set_features(ret, i,
-                                          gst_caps_features_from_string(GST_CAPS_FEATURE_MEMORY_SECMEM_MEMORY));
-                }
+    incoming_caps_size = gst_caps_get_size (caps);
+
+    if (direction == GST_PAD_SINK) {
+        for (i = 0; i < incoming_caps_size; ++i) {
+            GstStructure *incoming_structure = gst_caps_get_structure (caps, i);
+            GstStructure *outgoing_structure = NULL;
+
+            if (!gst_structure_has_field (incoming_structure, "original-media-type"))
+                continue;
+
+            const gchar *media_type = gst_structure_get_string (incoming_structure, "original-media-type");
+            isvideo = g_str_has_prefix (media_type, "video");
+
+            outgoing_structure = gst_structure_copy (incoming_structure);
+            gst_structure_set_name (outgoing_structure, gst_structure_get_string (outgoing_structure, "original-media-type"));
+            GST_DEBUG_OBJECT(plugin, "transform_caps direction:%d caps:%" GST_PTR_FORMAT, direction, outgoing_structure);
+
+            gint n_fields = gst_structure_n_fields (incoming_structure);
+
+            /* filter out the DRM related fields from the down-stream caps */
+            for (gint j = 0; j < n_fields; ++j) {
+                const gchar *field_name;
+                field_name = gst_structure_nth_field_name (incoming_structure, j);
+
+                if (g_str_has_prefix(field_name, "protection-system")
+                    || g_str_has_prefix(field_name, "original-media-type")
+                    || g_str_has_prefix(field_name, "pixel-aspect-ratio"))
+                    gst_structure_remove_field(outgoing_structure, field_name);
             }
-            if (find) {
-                if (!plugin->allocator) {
-                    plugin->allocator = gst_secmem_allocator_new(false, false);
-                }
-                if (plugin->outcaps) {
-                    gst_caps_unref(plugin->outcaps);
-                }
 
-                plugin->outcaps = gst_caps_ref(ret);
-            } else {
-                gst_caps_unref(ret);
-                ret = gst_caps_copy(sinkcaps);
+            AppendIfNotDuplicate(transformed_caps, outgoing_structure);
+            GST_DEBUG_OBJECT(plugin, "transform_caps direction:%d caps:%" GST_PTR_FORMAT, direction, transformed_caps);
+
+            if (isvideo) //for drm sec
+                gst_caps_set_features (transformed_caps, i, gst_caps_features_from_string(GST_CAPS_FEATURE_MEMORY_SECMEM_MEMORY));
+        }
+    } else {
+        if (!gst_caps_is_empty(caps) && !gst_caps_is_any(caps)) {
+            for (int i = 0; i < incoming_caps_size; i++) {
+                GstStructure* in = gst_caps_get_structure(caps, i);
+                GstStructure* out = NULL;
+                GstStructure* tmp = gst_structure_copy(in);
+
+                /* filter out the video/audio related fields from the up-stream caps */
+                for (int index = gst_structure_n_fields(tmp) - 1; index >= 0; --index) {
+                    const gchar* fieldName = gst_structure_nth_field_name(tmp, index);
+                    if (!g_strcmp0(fieldName, "base-profile")
+                        || !g_strcmp0(fieldName, "codec_data")
+                        || !g_strcmp0(fieldName, "mpegversion")
+                        || !g_strcmp0(fieldName, "stream-format")
+                        || !g_strcmp0(fieldName, "framed")
+                        || !g_strcmp0(fieldName, "height")
+                        || !g_strcmp0(fieldName, "framerate")
+                        || !g_strcmp0(fieldName, "level")
+                        || !g_strcmp0(fieldName, "pixel-aspect-ratio")
+                        || !g_strcmp0(fieldName, "channels")
+                        || !g_strcmp0(fieldName, "profile")
+                        || !g_strcmp0(fieldName, "rate")
+                        || !g_strcmp0(fieldName, "width")) {
+                        gst_structure_remove_field(tmp, fieldName);
+                    }
+                }
+                out = gst_structure_copy(tmp);
+                gst_structure_set(out, "original-media-type", G_TYPE_STRING, gst_structure_get_name(in), NULL);
+                gst_structure_set_name(out, "application/x-hdcp");
+                AppendIfNotDuplicate(transformed_caps, out);
+                gst_structure_free(tmp);
             }
         } else {
-            ret = gst_caps_copy(sinkcaps);
+            transformed_caps = gst_caps_new_empty_simple("application/x-hdcp");
+            gst_caps_set_simple(transformed_caps, "original-media-type", G_TYPE_STRING, "video/x-h264", NULL);
+            gst_caps_append_structure(transformed_caps, gst_structure_new ("application/x-hdcp",
+                                    "original-media-type", G_TYPE_STRING, "audio/mpeg", NULL));
         }
-        break;
-    }
-    case GST_PAD_SRC:
-        if (plugin->outcaps != NULL && gst_caps_can_intersect(caps, plugin->outcaps)) {
-            ret = gst_caps_copy(caps);
-            unsigned size = gst_caps_get_size(ret);
-            for (unsigned i = 0; i < size; ++i) {
-                gst_caps_set_features(ret, i, NULL);
-            }
-        } else if (gst_caps_can_intersect(caps, srccaps))
-            ret = gst_caps_copy(caps);
-        else
-            ret = gst_caps_copy(srccaps);
-        break;
-    default:
-        g_assert_not_reached();
     }
 
     if (filter) {
         GstCaps *intersection;
-        intersection = gst_caps_intersect_full(filter, ret, GST_CAPS_INTERSECT_FIRST);
-        gst_caps_unref(ret);
-        ret = intersection;
+        GST_DEBUG_OBJECT (plugin, "Using filter caps %" GST_PTR_FORMAT, filter);
+        intersection =
+            gst_caps_intersect_full (transformed_caps, filter,
+            GST_CAPS_INTERSECT_FIRST);
+        gst_caps_replace (&transformed_caps, intersection);
     }
-
-    GST_DEBUG_OBJECT (plugin, "transform_caps result: caps:%" GST_PTR_FORMAT, ret);
-    gst_caps_unref(srccaps);
-    gst_caps_unref(sinkcaps);
-    return ret;
+    GST_DEBUG_OBJECT (plugin, "amlhdcp returning %" GST_PTR_FORMAT, transformed_caps);
+    return transformed_caps;
 }
 
 gboolean
@@ -235,15 +294,26 @@ gst_aml_prepare_output_buffer(GstBaseTransform * trans, GstBuffer *inbuf, GstBuf
 {
     GstAmlHDCP *plugin = GST_AMLHDCP(trans);
     GstFlowReturn ret = GST_FLOW_OK;
+    gboolean isvideo = FALSE;
+    gboolean secure = FALSE;
+    GstProtectionMeta *meta = gst_buffer_get_protection_meta(inbuf);
+    if (meta) {
+        GST_DEBUG_OBJECT(plugin, "gst_aml_prepare_output_buffer meta:%" GST_PTR_FORMAT, meta->info);
+        gst_structure_get_boolean(meta->info, "isvideo", &isvideo);
+        gst_structure_get_boolean(meta->info, "secure", &secure);
+    }
 
-    g_return_val_if_fail(plugin->allocator != NULL, GST_FLOW_ERROR);
+    if (!isvideo || !secure) {
+        *outbuf = gst_buffer_new_allocate(NULL, gst_buffer_get_size(inbuf), NULL);
+    } else {
+        if (!plugin->allocator)
+            plugin->allocator = gst_secmem_allocator_new(true, false);
 
-    *outbuf = gst_buffer_new_allocate(plugin->allocator, gst_buffer_get_size(inbuf), NULL);
+        g_return_val_if_fail(plugin->allocator != NULL, GST_FLOW_ERROR);
+        *outbuf = gst_buffer_new_allocate(plugin->allocator, gst_buffer_get_size(inbuf), NULL);
+    }
 
-    g_return_val_if_fail(*outbuf != NULL, GST_FLOW_ERROR);
-
-    GST_BASE_TRANSFORM_CLASS(parent_class)->copy_metadata (trans, inbuf, *outbuf);
-
+    GST_BASE_TRANSFORM_CLASS(parent_class)->copy_metadata(trans, inbuf, *outbuf);
     return ret;
 }
 
@@ -252,7 +322,8 @@ gst_aml_hdcp_transform(GstBaseTransform *trans, GstBuffer *inbuf, GstBuffer *out
 {
     GstAmlHDCP *plugin = GST_AMLHDCP(trans);
     GstFlowReturn ret = GST_FLOW_OK;
-    bool secure = false;
+    gboolean secure = FALSE;
+    gboolean isvideo = FALSE;
     GstBuffer *iv_buffer;
     GValue *value;
     GstMapInfo map;
@@ -260,6 +331,7 @@ gst_aml_hdcp_transform(GstBaseTransform *trans, GstBuffer *inbuf, GstBuffer *out
     GstProtectionMeta *meta = gst_buffer_get_protection_meta(inbuf);
     if (meta) {
         GstStructure *drm_info = meta->info;
+        gst_structure_get_boolean(meta->info, "isvideo", &isvideo);
         if (!gst_structure_get(drm_info, "secure", G_TYPE_BOOLEAN,
                 &secure, NULL)) {
             GST_INFO_OBJECT(plugin, "No drm_info, take it as clean!!!");
@@ -292,9 +364,18 @@ gst_aml_hdcp_transform(GstBaseTransform *trans, GstBuffer *inbuf, GstBuffer *out
             ret = GST_FLOW_ERROR;
         }
         gst_buffer_unmap(iv_buffer, &map);
-    }else { // for have hdcp protocol but no iv
+    } else if(isvideo) { // for have hdcp protocol but no iv
         ret = gst_buffer_copy_to_secmem(outbuf, inbuf) ? GST_FLOW_OK : GST_FLOW_ERROR;
         GST_INFO_OBJECT(plugin, "for have hdcp protocol but no iv secmem ret %d", ret);
+    } else {
+        GstMapInfo inmap, outmap;
+        gst_buffer_map(inbuf, &inmap, GST_MAP_READ);
+        gst_buffer_map(outbuf, &outmap, GST_MAP_READWRITE);
+        memcpy(outmap.data, inmap.data, inmap.size);
+
+        gst_buffer_unmap(inbuf, &inmap);
+        gst_buffer_unmap(outbuf, &outmap);
+        ret = GST_FLOW_OK;
     }
     return ret;
 }
@@ -384,26 +465,39 @@ static GstFlowReturn drm_hdcp_decrypt_trust_zone(GstAmlHDCP* drm, amlWfdHdcpHand
 {
     GstFlowReturn ret;
     GstMapInfo map;
+    GstMapInfo outmap;
     struct amlWfdHdcpDataInfo info = {0};
     GstMemory *mem;
     gsize size;
+    gsize outsize;
     amlWfdHdcpResultType err;
     unsigned long paddr;
-    mem = gst_buffer_peek_memory(outbuf, 0);
-    g_return_val_if_fail(gst_is_secmem_memory(mem), GST_FLOW_ERROR);
+    gboolean isvideo = FALSE;
 
-    size = gst_memory_get_sizes(mem, NULL, NULL);
+    GstProtectionMeta *meta = gst_buffer_get_protection_meta(inbuf);
+    if (meta)
+        gst_structure_get_boolean(meta->info, "isvideo", &isvideo);
+
+    if (isvideo) {
+        mem = gst_buffer_peek_memory(outbuf, 0);
+        g_return_val_if_fail(gst_is_secmem_memory(mem), GST_FLOW_ERROR);
+        size = gst_memory_get_sizes(mem, NULL, NULL);
+        paddr = (unsigned long)gst_secmem_memory_get_handle(mem);
+        info.isAudio = 0;
+        info.out = (uint8_t *)paddr;
+    } else {
+        gst_buffer_map(outbuf, &outmap, GST_MAP_READWRITE);
+        size = outmap.size;
+        info.isAudio = 1;
+        info.out = outmap.data;
+    }
+
     gst_buffer_map(inbuf, &map, GST_MAP_READ);
     if (map.size != size) {
         GST_ERROR("buffer size not match");
         goto beach;
     }
-    GST_ERROR("here %d", map.size);
-    paddr = (unsigned long)gst_secmem_memory_get_handle(mem);
-
-    info.isAudio = 0;
     info.in = map.data;
-    info.out = (uint8_t *)paddr;
     info.inSize = map.size;
     info.outSize = map.size;
     info.streamCtr = stream_ctr;
@@ -446,7 +540,6 @@ GstFlowReturn drm_hdcp_decrypt_all(GstAmlHDCP *plugin, GstBuffer *inbuf, GstBuff
             }
         }
     }
-    GST_ERROR("here");
     hdcp = (plugin->static_pipeline) ? &plugin->hdcp_handle : plugin->hdcp_context;
     unsigned int stream_ctr = drm_hdcp_calc_stream_ctr(iv);
     unsigned long long input_ctr = drm_hdcp_calc_input_ctr(iv);
